f4b7ccc2d4e4c86f3b4486066fbbe7d7cae72e8d
cottonlist <35421808+cottonlist@users.noreply.github.com>
2020-05-18 00:18:05 +0900

    project 5

---
+++ project_5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int rank;
+	char *suit;
+}card;
+
+int main(int argc, char const *argv[])
+{
+	// card *card1 = (card *) malloc(sizeof(card));
+	// card1->rank = 1;
+	// printf("%d\n", card1->rank);
+
+	card *cards[52];
+
+	for (int i = 0; i < 13; ++i)
+	{
+		cards[i] = (card *) malloc(sizeof(card));
+		cards[i]->rank = i + 1;
+		cards[i]->suit = "spades";
+	}
+
+	for (int i = 13; i < 26; ++i)
+	{
+		cards[i] = (card *) malloc(sizeof(card));
+		cards[i]->rank = i - 12;
+		cards[i]->suit = "hearts";
+	}
+
+	for (int i = 26; i < 39; ++i)
+	{
+		cards[i] = (card *) malloc(sizeof(card));
+		cards[i]->rank = i - 25;
+		cards[i]->suit = "clubs";
+	}
+
+	for (int i = 39; i < 52; ++i)
+	{
+		cards[i] = (card *) malloc(sizeof(card));
+		cards[i]->rank = i - 38;
+		cards[i]->suit = "diamonds";
+	}
+
+	for (int i = 0; i < 5; ++i)
+	{
+		int index = rand() % 52 + 1;
+		printf("%d, %s\n", cards[index]->rank, cards[index]->suit);
+	}
+
+	return 0;
+}
